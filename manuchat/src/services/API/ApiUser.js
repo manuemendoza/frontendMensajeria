94962e092b30e9e6fcf6e74fe33065a9168bd748
Manuel Mendoza <77455281+manuemendoza@users.noreply.github.com>
2021-12-14 13:57:37 +0100

    i finished the user login part with the login API and am starting to create part of the registry

---
+++ manuchat/src/services/API/ApiUser.js
@@ -0,0 +1,21 @@
+export const ApiUser = {
+    loginUser: async (email, password) => {
+        try {
+            let result = await fetch(`http://localhost:9525/users/login`, {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password
+                })
+            });
+            
+            const userData = await result.json();
+            console.log(userData.token);
+            console.log(userData.userData);
+            return userData;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
