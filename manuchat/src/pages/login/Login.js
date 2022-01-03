d1607eb476ed49a9fc0a78e914818977e16f693f
Manuel Mendoza <77455281+manuemendoza@users.noreply.github.com>
2022-01-03 17:42:51 +0100

    updated the login component adding some functionalities like save to local storage

--- manuchat/src/pages/login/Login.js
+++ manuchat/src/pages/login/Login.js
@@ -1,37 +1,54 @@
+import "./Login.scss"
 import Button from "../../components/button/Button";
 import { ApiUser } from "../../services/API/ApiUser";
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import imagenLogo from '../../img/ManudChat_logo_p.png';
 
 const Login = () => {
-    const navigate = useNavigate()
-    const redirectionToRegister = () => {
-        navigate("/register");
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+    
+    const redirectionToClient = () => {
+        navigate("/chat");
     };
+    
+    useEffect(()=>{
+        if (token) {
+            redirectionToClient();
+        }
+    },[]);
+
     const handleSendData = async (e) =>{
-        e.preventDefault()
+        e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email);
-        console.log(password);
         try {
             const res = await ApiUser.loginUser(email, password);
             const token = res.token;
             const user = res.userData;
-            console.log(token);
-            console.log(user);
+            localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
+            if (token.length > 0) { 
+                redirectionToClient();
+            } else {   
+                console.log(null);
+            };
         } catch (error) {
-            alert('no esta funcionando')
+            alert(error, ' no esta funcionando');
         }
     };
+    
     return(
         <>
-        <div className="Profile">
+        <div className="container">
+            <img src={imagenLogo} alt="Imagen Logo" className='logo_login'/>
+            <h1 className="login_title">Iniciar Sesión</h1>
             <form onSubmit={(e) => handleSendData(e)}> 
-                
                 <div>
                     <div>
                         <label>
-                            Email: 
                             <input
                                 type='email'
                                 name='email'
@@ -40,8 +57,7 @@ const Login = () => {
                         </label>
                     </div>
                     <div>
-                        <label>
-                            Password: 
+                        <label> 
                             <input
                                 type='password'
                                 name='password'
@@ -50,8 +66,8 @@ const Login = () => {
                         </label>
                     </div>
                 </div>
-                <Button type="submit" >Entra</Button>
-                <Button onClick={()=>redirectionToRegister()} >Registrate</Button>
+                <p className="login_text">¿Aun no tienes Cuenta?<span><Link to="/register">Cree una</Link></span></p>
+                <Button type="submit" className="login_button" >Entra</Button>
             </form>
         </div>
         </>
