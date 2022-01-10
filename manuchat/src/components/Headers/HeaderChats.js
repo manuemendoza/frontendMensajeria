e4a7c0a8f8b451515a3df56a6ba501d8aea97c21
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2022-01-10 03:15:15 +0100

    created a header chat

--- manuchat/src/components/Headers/HeaderChats.js
+++ manuchat/src/components/Headers/HeaderChats.js
@@ -1,9 +1,24 @@
-const HeaderChats = () => {
+import Button from "../Button/Button";
+import Logout from "../Logout/Logout";
+import Search from "../Search/Search"
+import UpdateUser from "../Updates/UpdateUser";
+
+const HeaderChats = (props) => {
+    const userLocal = JSON.parse(localStorage.getItem("user"));
     return(
-        <>
-        <div></div>
-        <p>Este es el header del ChatPage</p>
-        </>
+        <div>
+            <Search></Search>
+            <Button>{userLocal.username}</Button>
+            <Logout></Logout>
+            <UpdateUser
+            id={userLocal.id}
+            name={userLocal.name}
+            surname={userLocal.surname}
+            username={userLocal.username}
+            email={userLocal.email} 
+            contacts={userLocal.contacts}
+            />
+        </div>
     )
 };
 
